e36b0548879a49063096bbb11840bc74ce4eec32
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-08-15 00:53:37 -0400

    pseudo code and initial JS code

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,14 @@
 //Crypto App API
 
-//
+//when user access website user should find value of chosen crypto currency in USD
+
+//get user input
+let userInput = document.getElementById('crypto-input').val()
+
+//take user input and return in current USD
+userInput.addEventListener('click', returnUSD)
+
+//collect and append data to second input field
+function returnUSD(){
+
+}
