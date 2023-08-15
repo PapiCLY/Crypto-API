8aaea8b84f5b6ff9dbc4977be0c8585047603e04
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-08-15 01:24:57 -0400

    added notes and intial fetch JS

--- Assets/script.js
+++ Assets/script.js
@@ -3,12 +3,21 @@
 //when user access website user should find value of chosen crypto currency in USD
 
 //get user input
-let userInput = document.getElementById('crypto-input').val()
+
+let userInput = document.getElementById('userQuery')
 
 //take user input and return in current USD
+
 userInput.addEventListener('click', returnUSD)
 
 //collect and append data to second input field
+
 function returnUSD(){
+
+ //grab value of user input   
+let query = $('#input').val()
+
+//place API URL in variable
+let apiURL = 
 
 }
