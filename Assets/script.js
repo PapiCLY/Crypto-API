26cd57e778913dfc29718217785e812d6279d69b
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-08-15 21:03:48 -0400

    updated JS API

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,25 @@
 //Crypto App API
-
+var icon= ''; 
+var coinName='';
+var price='';
 //when user access website user should find value of chosen crypto currency in USD
 
 //get user input
 
-let userInput = document.getElementById('userQuery')
+let userInput = document.getElementById('userQuery');
 
 //take user input and return in current USD
 
-userInput.addEventListener('click', returnUSD)
+userInput.addEventListener('click', function(){
+    returnUSD();
+})
 
 //collect and append data to second input field
 
 function returnUSD(){
 
  //grab value of user input   
-let query = $('#input').val()
+var query = $('#userInput').val();
 
 //place API URL in variable
 let apiURL = `https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&search=${query}&orderDirection=desc&limit=50&offset=0`;
@@ -26,14 +30,17 @@ headers: {
 } }).then(function(response){
 if(response.ok){
 response.json().then(function(data){
-let icon = data.data.coins[0].iconURL;
-let name = data.data.coins[0].name;
-let price = data.data.coins[0].price;
+icon = data.data.coins[0].iconURL;
+coinName = data.data.coins[0].name;
+price = data.data.coins[0].price;
 
-$('#icon').html(icon);
+$('#icon').attr('src', icon);
+$('#name').html(coinName);
+$('#price').html(price);
 
 })
 }
 })
 
 }
+
